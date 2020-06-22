418013d9cd9b96730fae1ac6ac95a4e38d6f6aa6
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-22 21:17:07 +0200

    feat(Product List): basic layout

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,11 +5,21 @@ import { Col } from '~/components/Grid';
 import {
     wrapper,
     sectionTitle,
+    productList,
+    product,
 } from './ProductList.module.scss';
 
 const ProductList = () => (
     <Col className={wrapper}>
         <h2 className={sectionTitle}>Products</h2>
+        <ul className={productList}>
+            <li className={product} />
+            <li className={product} />
+            <li className={product} />
+            <li className={product} />
+            <li className={product} />
+            <li className={product} />
+        </ul>
     </Col>
 );
 
