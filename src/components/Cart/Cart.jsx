bba15339f2a4fee31323bd6f2fed627e7edb4170
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-06-23 09:54:39 +0200

    feat(Cart): ids to products map

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { products as productsSelector } from '~/redux/selectors/productsSelectors';
+import { cart as cartSelector } from '~/redux/selectors/cartSelectors';
 
 import { Col } from '~/components/Grid';
 
 import { wrapper, sectionTitle } from './Cart.module.scss';
 
-const Cart = () => (
-    <Col className={wrapper}>
-        <h2 className={sectionTitle}>Cart</h2>
-    </Col>
-);
+const Cart = () => {
+    const { list } = useSelector(productsSelector); // products
+    const { items } = useSelector(cartSelector); // ids from cart
+
+    const cartItems = list.map((product) => ({
+        id: product.id,
+        cover: product.cover,
+        name: product.name,
+        price: product.price,
+        curency: product.currency,
+        quantity: items.filter((id) => id === product.id).length,
+    })).filter((cartItem) => cartItem.quantity !== 0);
+
+    console.log(cartItems);
+
+    return (
+        <Col className={wrapper}>
+            <h2 className={sectionTitle}>Cart</h2>
+        </Col>
+    );
+};
 
 export default Cart;
